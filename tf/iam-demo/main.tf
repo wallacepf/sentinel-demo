52db0e7fa0dd4726a94be4f1f4685f7407e57b08
wallacepf <wallacepf@gmail.com>
2022-01-17 12:01:37 -0300

    General Changes

---
+++ tf/iam-demo/main.tf
@@ -0,0 +1,23 @@
+terraform {
+    cloud {
+        organization = "my-demo-account"
+
+        workspaces {
+            name = "sentinel-iam-demo"
+        }
+    }
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+module "iam_user2" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-user"
+  version = "4.9.0"
+  
+  name = "dummy"
+  force_destroy = true 
+    create_iam_user_login_profile = false
+  create_iam_access_key         = true
+}
