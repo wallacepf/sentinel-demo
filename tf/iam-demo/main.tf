876208d7fc889a228bac282ef3c3e0a71e4ba4fb
wallacepf <wallacepf@gmail.com>
2022-01-17 14:53:08 -0300

    Changes in tf file

--- tf/iam-demo/main.tf
+++ tf/iam-demo/main.tf
@@ -12,12 +12,16 @@ provider "aws" {
     region = "us-east-1"
 }
 
-module "iam_user2" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-user"
-  version = "4.9.0"
+// module "iam_user2" {
+//   source  = "terraform-aws-modules/iam/aws//modules/iam-user"
+//   version = "4.9.0"
   
-  name = "alfred"
-  force_destroy = true 
-    create_iam_user_login_profile = false
-  create_iam_access_key         = true
+//   name = "dummy"
+//   force_destroy = true 
+//     create_iam_user_login_profile = false
+//   create_iam_access_key         = true
+// }
+
+resource "null_resource" "teste" {
+    
 }
