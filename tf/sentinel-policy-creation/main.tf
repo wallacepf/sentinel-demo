a88154f35813f0f0b50b3b0ef2fa5eacc1a9a31d
wallacepf <wallacepf@gmail.com>
2022-01-18 13:49:13 -0300

    General Fixes in tf file and pipeline

--- tf/sentinel-policy-creation/main.tf
+++ tf/sentinel-policy-creation/main.tf
@@ -12,15 +12,28 @@ provider "tfe" {
   token = var.token
 }
 
-resource "tfe_policy_set" "test" {
-  name         = "deny-iam-user-creation"
-  description  = "Policy Set to Deny IAM account creation"
-  organization = var.org
+resource "tfe_policy_set" "deny-iam-user-creation" {
+  name          = "deny-iam-user-creation"
+  description   = "Policy Set to Deny IAM account creation"
+  organization  = var.org
+  policies_path = "policies/block-iam-account-creation"
 
   vcs_repo {
     identifier         = "wallacepf/sentinel-demo"
-    branch             = "v2"
+    branch             = "add-mono-policy"
     ingress_submodules = false
     oauth_token_id     = var.vcs_oauth_key
   }
 }
+
+// data "local_file" "block-iam" {
+//   filename = "block-iam-account-creation.sentinel"
+// }
+
+// resource "tfe_sentinel_policy" "block-iam" {
+//   name         = "block-iam"
+//   description  = "Policy to block IAM Account Creation"
+//   organization = var.org
+//   policy       = file("../../block-iam-account-creation.sentinel")
+//   enforce_mode = "hard-mandatory"
+// }
