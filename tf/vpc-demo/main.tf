92a09e1d1f4cc07a6e8eba29135c1ef20568f9f1
joseaugustolima <joseaugusto.mlima@gmail.com>
2022-01-20 11:20:45 -0300

    Commit

--- tf/vpc-demo/main.tf
+++ tf/vpc-demo/main.tf
@@ -1,7 +1,6 @@
 terraform {
-    cloud {
+    backend "remote" {
         organization = "my-demo-account"
-
         workspaces {
             name = "sentinel-vpc-demo"
         }
@@ -12,12 +11,12 @@ provider "aws" {
     region = "us-east-1"
 }
   
-  module "vpc" {
+module "vpc" {
     source = "terraform-aws-modules/vpc/aws"
 
     name = "my-vpc"
     cidr = "10.0.0.0/16"
-    
+
     azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
     private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
     public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
@@ -25,22 +24,28 @@ provider "aws" {
     enable_nat_gateway = true
 
     tags = {
-        XXX = "yyy"
+        Name = "Demo"
     }
-  }
+}
 
-  module "security_group" {
-    source  = "terraform-aws-modules/security-group/aws"
+module "security_group" {
+    source = "terraform-aws-modules/security-group/aws"
 
-    name        = "my-sg"
-    vpc_id      = module.vpc.vpc_id
+    name   = "my-sg"
+    vpc_id = module.vpc.vpc_id
 
     ingress_cidr_blocks = ["0.0.0.0/0"]
     ingress_rules       = ["http-80-tcp", "all-icmp"]
     egress_rules        = ["all-all"]
-
 }
 
-resource "null_resource" "teste" {
-    
+resource "aws_instance" "demo_server" {
+    ami           = "ami-02e136e904f3da870"
+    instance_type = "t2.micro"
+    availability_zone = element(module.vpc.azs, 0)
+    subnet_id = element(module.vpc.private_subnets, 0)
+
+    tags = {
+        Name = "Demo"
+    }
 }
