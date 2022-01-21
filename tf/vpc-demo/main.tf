128c421993e59b1dcb652b806158fdff2a1230a2
wallacepf <wallacepf@gmail.com>
2022-01-21 20:02:04 -0300

    Ran TF fmt

--- tf/vpc-demo/main.tf
+++ tf/vpc-demo/main.tf
@@ -1,51 +1,51 @@
 terraform {
-    backend "remote" {
-        organization = "my-demo-account"
-        workspaces {
-            name = "sentinel-vpc-demo"
-        }
+  backend "remote" {
+    organization = "my-demo-account"
+    workspaces {
+      name = "sentinel-vpc-demo"
     }
+  }
 }
 
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
-  
+
 module "vpc" {
-    source = "terraform-aws-modules/vpc/aws"
+  source = "terraform-aws-modules/vpc/aws"
 
-    name = "my-vpc"
-    cidr = "10.0.0.0/16"
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
 
-    azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
-    private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-    public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-    enable_nat_gateway = true
+  enable_nat_gateway = true
 
-    tags = {
-        Name = "Demo"
-    }
+  tags = {
+    Name = "Demo"
+  }
 }
 
 module "security_group" {
-    source = "terraform-aws-modules/security-group/aws"
+  source = "terraform-aws-modules/security-group/aws"
 
-    name   = "my-sg"
-    vpc_id = module.vpc.vpc_id
+  name   = "my-sg"
+  vpc_id = module.vpc.vpc_id
 
-    ingress_cidr_blocks = ["0.0.0.0/0"]
-    ingress_rules       = ["http-80-tcp", "all-icmp"]
-    egress_rules        = ["all-all"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules       = ["http-80-tcp", "all-icmp"]
+  egress_rules        = ["all-all"]
 }
 
 resource "aws_instance" "demo_server" {
-    ami           = "ami-02e136e904f3da870"
-    instance_type = "t2.micro"
-    availability_zone = element(module.vpc.azs, 0)
-    subnet_id = element(module.vpc.private_subnets, 0)
+  ami               = "ami-02e136e904f3da870"
+  instance_type     = "t2.micro"
+  availability_zone = element(module.vpc.azs, 0)
+  subnet_id         = element(module.vpc.private_subnets, 0)
 
-    tags = {
-        Name = "Demo"
-    }
+  tags = {
+    Name = "Demo"
+  }
 }
--- tf/vpc-demo/outputs.tf
+++ tf/vpc-demo/outputs.tf
@@ -1,4 +1,4 @@
 output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
-  value = aws_instance.demo_server.public_ip
+  value       = aws_instance.demo_server.public_ip
 }
