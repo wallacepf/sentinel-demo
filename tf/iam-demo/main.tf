6fc97c81c09855dbd1d413088f9a84c9b630fb1e
wallacepf <wallacepf@gmail.com>
2022-01-25 00:25:30 -0300

    General Fixes

--- tf/iam-demo/main.tf
+++ tf/iam-demo/main.tf
@@ -2,18 +2,22 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "random_pet" "bucket" {
+
+}
+
 // module "iam_user2" {
 //   source  = "terraform-aws-modules/iam/aws//modules/iam-user"
 //   version = "4.9.0"
 
-//   name                          = "dummy"
+//   name                          = "dummy-${random_pet.bucket.id}"
 //   force_destroy                 = true
 //   create_iam_user_login_profile = false
 //   create_iam_access_key         = true
 // }
 
 // resource "aws_iam_user" "dummer" {
-//     name = "dummer"
+//     name = "dummer-${random_pet.bucket.id}"
 //     force_destroy = true
 // }
 
