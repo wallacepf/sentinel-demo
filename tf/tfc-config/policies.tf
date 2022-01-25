76ebfab82011556685994d289734d25b79b45f26
wallacepf <wallacepf@gmail.com>
2022-01-25 01:05:41 -0300

    General Fixes

--- tf/tfc-config/policies.tf
+++ tf/tfc-config/policies.tf
@@ -17,7 +17,7 @@ data "tfe_workspace" "eip_demo" {
   name         = "sentinel-eip-demo-${local.env}"
   organization = local.org
 }
-  
+
 data "tfe_workspace_ids" "prod_wks" {
   tag_names    = ["prod"]
   organization = local.org
