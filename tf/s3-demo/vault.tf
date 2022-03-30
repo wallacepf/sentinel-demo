86de50edd6d454315831aaf2990946fd6ff4b375
wallacepf <wallacepf@gmail.com>
2022-03-30 02:07:03 -0300

    General Fixes

--- tf/s3-demo/vault.tf
+++ tf/s3-demo/vault.tf
@@ -1,6 +1,4 @@
-provider "vault" {
-  address = "https://vault-cluster.vault.5bbc50e3-a284-4743-877e-ffd388d684f2.aws.hashicorp.cloud:8200"
-}
+provider "vault" {}
 
 data "vault_aws_access_credentials" "creds" {
   backend = "aws"
