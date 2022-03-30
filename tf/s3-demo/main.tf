d36aae67e722937e3925e0f46c8c7dd23f711eb8
wallacepf <wallacepf@gmail.com>
2022-03-30 01:32:47 -0300

    Fixes

--- tf/s3-demo/main.tf
+++ tf/s3-demo/main.tf
@@ -1,8 +1,4 @@
-provider "vault" {
-  address = "https://vault-cluster.vault.5bbc50e3-a284-4743-877e-ffd388d684f2.aws.hashicorp.cloud:8200"
-  token = "s.HEeALONKTVyMj0ckx4IbsUTN.xbY3N"
-  skip_child_token = true
-}
+
 
 provider "aws" {
   region = "us-east-1"
@@ -11,10 +7,7 @@ provider "aws" {
 }
 
 
-data "vault_aws_access_credentials" "creds" {
-  backend = "aws"
-  role    = "tf-sentinel-demo"
-}
+
 
 resource "random_pet" "random" {
 
