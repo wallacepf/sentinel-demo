482f503f790c4436a189167be8a0e37dee468bd8
wallacepf <wallacepf@gmail.com>
2022-03-29 23:44:07 -0300

    Rolling back configs

--- tf/s3-demo/main.tf
+++ tf/s3-demo/main.tf
@@ -20,12 +20,12 @@ module "s3_bucket" {
   force_destroy = true
 
   versioning = {
-    enabled = true
+    enabled = false
   }
 
   tags = {
      Owner   = "Dev"
-     Purpose = "Demo"
+     Puurpose = "Demo"
   }
 
   server_side_encryption_configuration = {
