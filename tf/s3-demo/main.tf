8ee5afd6d40c9c1e272f67426bbb49d8fdf4f2ba
wallacepf <wallacepf@gmail.com>
2022-03-30 01:22:17 -0300

    Fixes

--- tf/s3-demo/main.tf
+++ tf/s3-demo/main.tf
@@ -1,6 +1,7 @@
 provider "vault" {
   address = "https://vault-cluster.vault.5bbc50e3-a284-4743-877e-ffd388d684f2.aws.hashicorp.cloud:8200"
   token = "s.HEeALONKTVyMj0ckx4IbsUTN.xbY3N"
+  skip_child_token = true
 }
 
 provider "aws" {
@@ -18,7 +19,6 @@ data "vault_aws_access_credentials" "creds" {
 resource "random_pet" "random" {
 
 }
-
 
 resource "aws_kms_key" "objects" {
   description             = "KMS key is used to encrypt bucket objects"
