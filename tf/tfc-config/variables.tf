bfe0f326e62fe69d7091b30c57f738771a4667bd
wallacepf <wallacepf@gmail.com>
2022-03-30 02:07:38 -0300

    Fixes

--- tf/tfc-config/variables.tf
+++ tf/tfc-config/variables.tf
@@ -3,3 +3,11 @@ variable "aws_access_key" { sensitive = true }
 variable "aws_secret_id" { sensitive = true }
 variable "token" { sensitive = true }
 variable "branch" {}
+variable "vault_token" { 
+    sensitive = true 
+    default = "mytoken"
+    }
+variable "vault_addr" {
+    sensitive = true
+    default = "http://127.0.0.1:8200"
+    }
