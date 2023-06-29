1bd03fe34acf33b8da302ef22de4bf5c880ed136
wallacepf <wallacepf@me.com>
2023-06-29 16:41:34 -0300

    Commenting module nocode

--- tf/tfc-config/modules.tf
+++ tf/tfc-config/modules.tf
@@ -1,7 +1,7 @@
-resource "tfe_registry_module" "rds-module" {
-  vcs_repo {
-    display_identifier = "wallacepf/terraform-aws-rds"
-    identifier         = "wallacepf/terraform-aws-rds"
-    oauth_token_id     = var.vcs_oauth_key
-  }
-}
+# resource "tfe_registry_module" "rds-module" {
+#   vcs_repo {
+#     display_identifier = "wallacepf/terraform-aws-rds"
+#     identifier         = "wallacepf/terraform-aws-rds"
+#     oauth_token_id     = var.vcs_oauth_key
+#   }
+# }
