1b68c3f254fc0d8834f731cb181570f6505e25a7
wallacepf <wallacepf@me.com>
2023-06-29 14:18:41 -0300

    Fixing Module declaration

--- tf/tfc-config/modules.tf
+++ tf/tfc-config/modules.tf
@@ -1,7 +1,7 @@
 resource "tfe_registry_module" "rds-module" {
   vcs_repo {
-    display_identifier = "wallacepf/terraform-provider-name"
-    identifier         = "wallacepf/terraform-provider-name"
+    display_identifier = "wallacepf/terraform-aws-rd"
+    identifier         = "wallacepf/terraform-provider-rds"
     oauth_token_id     = var.vcs_oauth_key
   }
 }
