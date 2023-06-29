564e63485310c4ce14c4611e7d0321c51d0eb572
wallacepf <wallacepf@me.com>
2023-06-29 11:23:07 -0300

    Changing Workspace Configs

--- tf/tfc-config/workspaces.tf
+++ tf/tfc-config/workspaces.tf
@@ -8,6 +8,7 @@ resource "tfe_workspace" "iam_demo" {
   organization      = local.org
   tag_names         = ["demo", "iam", "sentinel", local.env]
   auto_apply        = true
+  queue_all_runs = false
   working_directory = "tf/iam-demo"
   project_id = tfe_project.demo-29062023.id
 
@@ -16,12 +17,15 @@ resource "tfe_workspace" "iam_demo" {
     branch         = local.branch
     oauth_token_id = var.vcs_oauth_key
   }
+
+  depends_on = [ tfe_variable_set.vset-jit ]
 }
 
 resource "tfe_workspace" "s3_demo" {
   name              = "sentinel-s3-demo-${local.env}"
   organization      = local.org
-  auto_apply        = true
+  auto_apply        = false
+  queue_all_runs = false
   tag_names         = ["demo", "s3", "sentinel", local.env]
   working_directory = "tf/s3-demo"
   project_id = tfe_project.demo-29062023.id
@@ -31,12 +35,15 @@ resource "tfe_workspace" "s3_demo" {
     branch         = local.branch
     oauth_token_id = var.vcs_oauth_key
   }
+
+  depends_on = [ tfe_variable_set.vset-jit ]
 }
 
 resource "tfe_workspace" "vpc_demo" {
   name              = "sentinel-vpc-demo-${local.env}"
   organization      = local.org
   auto_apply        = true
+  queue_all_runs = false
   tag_names         = ["demo", "vpc", "sentinel", local.env]
   working_directory = "tf/vpc-demo"
   project_id = tfe_project.demo-29062023.id
@@ -46,6 +53,8 @@ resource "tfe_workspace" "vpc_demo" {
     branch         = local.branch
     oauth_token_id = var.vcs_oauth_key
   }
+
+  depends_on = [ tfe_variable_set.vset-jit ]
 }
 
 // resource "tfe_workspace" "eip_demo" {
